cf7ce933871ce3bd8f5cdd5b6f3f0c125ffce390
Shalini0106 <81474830+Shalini0106@users.noreply.github.com>
2026-08-21 17:09:06 +0530

    Python

---
+++ app.py
@@ -0,0 +1,13 @@
+"""
+Dependencies:
+    pip install pandas matplotlib seaborn plotly
+"""
+
+def main():
+    # Add your application code here
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
